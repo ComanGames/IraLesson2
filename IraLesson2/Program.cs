ea8404498cd797cf565b436b5de76a9070c45926
ComanGames <alfaa.gen@gmail.com>
2016-07-28 19:46:31 +0300

    Matematical opperations

--- IraLesson2/Program.cs
+++ IraLesson2/Program.cs
@@ -7,26 +7,21 @@ namespace IraLesson2
     {
         static void Main(string[] args)
         {
-            //Hello yura
-            //Hello Ira
-            string s = "10";
-            int a = 5;
-            s = a.ToString();//convert int to string
-            a = int.Parse(s); ///convert string to int
-            string s1 = "5.00001";
-            double d1 = double.Parse(s1); ///convert string to double
-            short sh = 100;
-            int i = sh; // conert short to int
-            sh = (short)i; // convert int to short
+            int a = 5 + 6;//11
+            a = a + 5;//16
+            a += 5;//21
+            a = a/2;
+            a /= 2;
+            a = a*2;
+            a *= 2;
+            a = a - 2;
+            a -= 2;
+            a = 5;// присвоэеня
             int b = 10;
-            double d = b;// int to double
-            b = (int)d;  // double to int
-
-            a = 5;
-            double d2 = a;
-            d2 += 0.001;
-            Console.WriteLine(d2);
+            Console.WriteLine(b%15);
             Console.ReadKey();
+            
+
         }
     }
 }
