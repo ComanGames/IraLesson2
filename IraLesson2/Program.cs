59587d07c8a486184f2ac045fdcd53a49d089be7
ComanGames <alfaa.gen@gmail.com>
2016-07-28 19:30:40 +0300

    Last Changes

--- IraLesson2/Program.cs
+++ IraLesson2/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.Design.Serialization;
 
 namespace IraLesson2
 {
@@ -10,6 +7,24 @@ namespace IraLesson2
     {
         static void Main(string[] args)
         {
+            string s = "10";
+            int a = 5;
+            s = a.ToString();//convert int to string
+            a = int.Parse(s); ///convert string to int
+            string s1 = "5.00001";
+            double d1 = double.Parse(s1); ///convert string to double
+            short sh = 100;
+            int i = sh; // conert short to int
+            sh = (short)i; // convert int to short
+            int b = 10;
+            double d = b;// int to double
+            b = (int)d;  // double to int
+
+            a = 5;
+            double d2 = a;
+            d2 += 0.001;
+            Console.WriteLine(d2);
+            Console.ReadKey();
         }
     }
 }
